4738e98d6aaa5ad5a9beedb24cfffca4c5349606
lhmontech <lhmonteiro.ti@gmail.com>
2025-10-09 13:03:20 -0300

    starting the interface skeleton

--- Calculator-frontend/src/App.js
+++ Calculator-frontend/src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='Menu'>
+        <header>
+          <h1>MENU</h1>
+          <p>
+            Hello, welcome to lhmontech calculator!
+            Select the option you want to calculate:
+          </p>
+        </header>
+        <div className='Menu_buttons'>
+          <div className='Left_buttons'>
+            <p>
+              Common Calculations
+            </p>
+            <p>
+              Fibonacci sequence
+            </p>
+            <p>
+              Factorial calculation
+            </p>
+            <p>
+              Perfect squares sequence
+            </p>
+            <p>
+              Prime numbers sequence
+            </p>
+            <p>
+              Conversion of decimal - binary
+            </p>
+          </div>
+          <div className='Right_buttons'>
+            <p>
+              Common Calculations
+            </p>
+            <p>
+              Fibonacci sequence
+            </p>
+            <p>
+              Factorial calculation
+            </p>
+            <p>
+              Perfect squares sequence
+            </p>
+            <p>
+              Prime numbers sequence
+            </p>
+            <p>
+              Conversion of decimal - binary
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='Result_area'>
+        <div className='Monitor'>
+
+        </div>
+        <div className='Forms'>
+
+        </div>
+        <div className='Keypad'>
+
+        </div>
+      </div>
     </div>
   );
 }
