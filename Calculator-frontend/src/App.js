623603130575db36482490a5d6ca8e79f42fedf5
Lucas Monteiro <lhmonteiro.ti@gmail.com>
2025-10-20 17:34:14 -0300

    Update App.js

--- Calculator-frontend/src/App.js
+++ Calculator-frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import { Home, Tribonacci, Fibonacci, Primes } from './pages/pages';
+import { Home, Alternating, Tribonacci, Fibonacci, Primes, Binary, Common, Cubes, Factorial, Geometric, Hexadecimal, Squares } from './pages/pages';
 
 function App() {
   return (
@@ -8,9 +8,17 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="alternating" element={<Alternating />} />
           <Route path="fibonacci" element={<Fibonacci />} />
           <Route path="tribonacci" element={<Tribonacci />} />
           <Route path="primes" element={<Primes />} />
+          <Route path="binary" element={<Binary />} />
+          <Route path="common" element={<Common />} />
+          <Route path="cubes" element={<Cubes />} />
+          <Route path="factorial" element={<Factorial />} />
+          <Route path="geometric" element={<Geometric />} />
+          <Route path="hexadecimal" element={<Hexadecimal />} />
+          <Route path="squares" element={<Squares />} />
         </Route>
       </Routes>
     </BrowserRouter>
